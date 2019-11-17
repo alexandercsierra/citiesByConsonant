bda524a5004df96ecd5c5c9ea39c3fbe7b1ba974
Alexander Sierra <alexandercsierra@gmail.com>
2019-11-17 18:24:56 -0500

    added states up to california

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+//code to extract cities from wikipedia.org
+
+var city = Array.prototype.slice.call(document.querySelectorAll('td > a'));
+
+var names = [];
+
+function findCities (arr){ for (let i=0; i<arr.length; i++) {if (typeof arr[i].innerHTML === "string"); {names.push(arr[i].innerHTML);}} return names;}
+
+findCities(city);
+
+console.log(JSON.stringify(names));
 
 
-// let alabama = ["stuff", "things", "Mobile", "limb", "MOBILllE", "MLBIOU"];
+
+
+
 
 
 let cities = [];
